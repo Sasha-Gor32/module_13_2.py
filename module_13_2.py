a9d9c53e6dcbc664ecb03c238cc5f8b94dd8bd82
Sasha-Gor32 <sasha051505@gmail.com>
2025-01-15 18:47:22 +0300

    Add files via upload

---
+++ module_13_2.py
@@ -0,0 +1,18 @@
+from aiogram import Bot, Dispatcher, types
+import asyncio
+from aiogram.filters import CommandStart
+api = "???????????????????????????"
+bot = Bot(token=api)
+dp = Dispatcher()
+@dp.message(CommandStart())
+async def start(message: types.Message):
+    await message.answer('Привет! Я бот, помогающий твоему здоровью.')
+    print('Привет! Я бот, помогающий твоему здоровью.')
+@dp.message()
+async def all_massages(message: types.Message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+    print('Введите команду /start, чтобы начать общение.')
+async def main():
+    await dp.start_polling(bot)
+if __name__ == "__main__":
+    asyncio.run(main())
