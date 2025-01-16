d282802cf6522626696520cd757db21f4a1b58c5
Sasha-Gor32 <sasha051505@gmail.com>
2025-01-16 18:04:14 +0300

    Update module_13_2.py

--- module_13_2.py
+++ module_13_2.py
@@ -6,11 +6,11 @@ bot = Bot(token=api)
 dp = Dispatcher()
 @dp.message(CommandStart())
 async def start(message: types.Message):
-    await message.answer('Привет! Я бот, помогающий твоему здоровью.')
+    #await message.answer('Привет! Я бот, помогающий твоему здоровью.')
     print('Привет! Я бот, помогающий твоему здоровью.')
 @dp.message()
 async def all_massages(message: types.Message):
-    await message.answer('Введите команду /start, чтобы начать общение.')
+    #await message.answer('Введите команду /start, чтобы начать общение.')
     print('Введите команду /start, чтобы начать общение.')
 async def main():
     await dp.start_polling(bot)
